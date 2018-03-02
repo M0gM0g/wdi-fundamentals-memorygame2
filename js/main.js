13a563d52510283dfc8b3a17f5f3d529cf6e2723
Mark O'Leary <markoleary@Marks-MBP.fios-router.home>
2018-03-01 23:29:40 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards [0];
+var cardTwo = cards [2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+
+		alert("You've found a match!");
+	} else {
+		alert("sorry, try again.");
+	}
+}
+
+
+
+
+
+
+
 
