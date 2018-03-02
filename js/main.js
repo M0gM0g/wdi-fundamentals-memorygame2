ecb7f94ab40bbf087272a180b4253d06c4c79da8
Mark O'Leary <markoleary@Marks-MBP.fios-router.home>
2018-03-02 01:12:25 -0500

    finished game

--- js/main.js
+++ js/main.js
@@ -1,23 +1,39 @@
-console.log("Up and running!");
 
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{ 
+	card: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+
+{
+	card: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+
+	card: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+
+{
+	card: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
-var cardOne = cards [0];
-var cardTwo = cards [2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
 
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 
@@ -25,12 +41,32 @@ if (cardsInPlay.length === 2) {
 	} else {
 		alert("sorry, try again.");
 	}
-}
+};
 
+var flipCard = function () {
+	cardId = this.getAttribute("data-id");
+	console.log(cardId);
+	cardsInPlay.push(cards[cardId].card);
+	this.setAttribute("src", cards[cardId].cardImage);
 
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		cardsInPlay = [];
+	}
+};
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board" ).appendChild(cardElement);
+	}
 
+} 
 
+createBoard();
 
 
 
